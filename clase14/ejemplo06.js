fcede89c5a184d23b4df5711dbcf1f3f95b383cf
Abril Maci <amaci003.am@gmail.com>
2022-10-12 12:01:05 -0300

    clase14

---
+++ clase14/ejemplo06.js
@@ -0,0 +1,7 @@
+const frutas = ['Manzanas','Bananas','Palta','Peras','Mandarinas'];
+/* tenemos manzanas 
+tenemos bananas  */
+
+for(let i=0; i<frutas.length; i++){
+    document.write('Tenemos ' + frutas[i] + '<br>')
+}
