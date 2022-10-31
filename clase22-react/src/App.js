943260844968bd7c4d6c6007306be0f3df104d4c
Abril Maci <amaci003.am@gmail.com>
2022-10-31 12:15:44 -0300

    Clase22react

---
+++ clase22-react/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import React from 'react';
+import Header from './components/layout/Header';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Header/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ clase22-react/src/components/layout/Header.js
@@ -0,0 +1,11 @@
+const Header = (props) =>{
+    return(
+    <div>
+        <p>Hola soy el Header</p>
+        <p>Lorem, aca iria un lorem q no funciona</p>
+        </div>
+
+    )
+}
+
+export default Header 
